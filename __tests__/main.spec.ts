7f2d0fb17dca63d33cbb12059ae91cee46ae97b3
Lee White <leewhite128@gmail.com>
2022-04-13 08:04:05 -0700

    chore: update prettier to support forwardRef (#30)

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,4 +1,5 @@
 import {convertSvgToReact} from '../src/main';
+import {fileTemplateReactForwardRef} from '../src/templates';
 
 const SRC_SVG = `<?xml version="1.0" encoding="utf-8"?>
 <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
@@ -31,7 +32,7 @@ export interface AutomaticIconProps {
 
 const AutomaticIcon = ({title = 'Automatic Icon', decorative = true, className, color, size}: AutomaticIconProps) => (
   <UID>
-    {titleId => (
+    {(titleId) => (
       <span style={{color, width: size, height: size, display: 'inline-block'}} className={className}>
         <svg
           role="img"
@@ -100,6 +101,56 @@ export default AutomaticIcon;
 `;
 
     const convertedSrc = await convertSvgToReact('AutomaticIcon', SRC_SVG, {useHooks: true});
+    expect(convertedSrc).toBe(result);
+  });
+
+  it('converts components with forwardRef correctly', async () => {
+    const result = `/**
+ * This file was automatically generated with @twilio-labs/svg-to-react
+ */
+import * as React from 'react';
+import {useUID} from 'react-uid';
+
+export interface AutomaticIconProps {
+  className?: string;
+  size?: number;
+  color?: string;
+  title?: string;
+  decorative?: boolean;
+}
+
+const AutomaticIcon = React.forwardRef<HTMLElement, AutomaticIconProps>(
+  ({title = 'Automatic Icon', decorative = true, className, color, size}, ref) => {
+    const titleId = useUID();
+
+    return (
+      <span style={{color, width: size, height: size, display: 'inline-block'}} className={className} ref={ref}>
+        <svg
+          role="img"
+          aria-hidden={decorative}
+          height="100%"
+          width="100%"
+          viewBox="0 0 24 24"
+          aria-labelledby={titleId}>
+          {title ? <title id={titleId}>{title}</title> : null}
+          <path
+            fill="currentColor"
+            d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8zm2.3-4.7H16L12.9 8h-1.7L8 15.3h1.7l.6-1.4h3.4l.6 1.4zm-3.5-2.7L12 9.7l1.2 2.8h-2.4v.1z"
+          />
+        </svg>
+      </span>
+    );
+  }
+);
+
+export default AutomaticIcon;
+`;
+
+    const convertedSrc = await convertSvgToReact('AutomaticIcon', SRC_SVG, {
+      template: fileTemplateReactForwardRef,
+      useHooks: false,
+    });
+
     expect(convertedSrc).toBe(result);
   });
 });
